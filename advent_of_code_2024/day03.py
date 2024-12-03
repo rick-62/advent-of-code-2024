08265f9d68c7b7028930024c7f71bdc96ab588ec
rick-62 <rick-62@hotmail.co.uk>
2024-12-03 08:48:23 +0000

    day03 part 2 complete

--- advent_of_code_2024/day03.py
+++ advent_of_code_2024/day03.py
@@ -13,11 +13,34 @@ def part1(contents):
     
     return total_sum
 
+
+
+def part2(contents):
+
+    # add "do()" to beginning of contents to ensure picked up by regex
+    contents = "do()" + contents
+
+    # Find all instances of "do()" and capture all characters up until "don't()"
+    pattern = re.compile(r'(do\(\).*?)(?:don\'t\(\)|$)', re.DOTALL)
+    matches = pattern.findall(contents)
+    reduced_contents = "".join(matches)
+
+    return part1(reduced_contents)
+
+
+
 if __name__ == "__main__":
     test_input = load_input('day03_test').read()
     test_solution = part1(test_input)
     assert test_solution == 161, test_solution
 
+    test_input = load_input('day03_test2').read()
+    test_solution = part2(test_input)
+    assert test_solution == 48, test_solution
+
     puzzle_input = load_input('day03').read()
     puzzle_solution = part1(puzzle_input)
+    print(puzzle_solution)
+
+    puzzle_solution = part2(puzzle_input)
     print(puzzle_solution)
