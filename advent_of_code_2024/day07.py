97a549126e1c0cd9e6c1977579cf1e7756f95c39
rick-62 <rick-62@hotmail.co.uk>
2024-12-07 20:14:23 +0000

    day07 part 2 complete

--- advent_of_code_2024/day07.py
+++ advent_of_code_2024/day07.py
@@ -29,14 +29,34 @@ def part1(data):
 
 
 def part2(data):
-    pass
+    total = 0
+    for value, numbers in parse_input(data):
+        combinations = itertools.product('*+|', repeat=len(numbers)-1)
+        for comb in combinations:
+            # Evaluate left-to-right manually
+            calc = int(numbers[0])
+            for i, op in enumerate(comb):
+                if op == '+':
+                    calc += int(numbers[i + 1])
+                elif op == '*':
+                    calc *= int(numbers[i + 1])
+                elif op == '|':
+                    calc = int(str(calc) + str(numbers[i + 1]))
+            if calc == value:
+                total += value
+                break
+    return total
 
 if __name__ == "__main__":
     test_input = load_input('day07_test').read()
     test_solution = part1(test_input)
     assert test_solution == 3749, f"Test failed, got {test_solution}"
+    test_solution = part2(test_input)
+    assert test_solution == 11387, f"Test failed, got {test_solution}"
 
     puzzle_input = load_input('day07').read()
     puzzle_solution = part1(puzzle_input)
+    print(puzzle_solution)
+    puzzle_solution = part2(puzzle_input)
     print(puzzle_solution)
 
