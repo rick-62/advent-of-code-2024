ad0804acd3a36c75948f669e538d44cab8992099
rick-62 <rick-62@hotmail.co.uk>
2024-12-09 19:43:49 +0000

    Update template to read inputs

--- advent_of_code_2024/template.py
+++ advent_of_code_2024/template.py
@@ -10,13 +10,13 @@ def part2(contents):
 
 if __name__ == "__main__":
 
-    test_input = load_input("day00_test")
+    test_input = load_input("day00_test").read()
     test_solution = part1(test_input)
     assert test_solution == None, test_solution
     test_solution = part2(test_input)
     assert test_solution == None, test_solution
 
-    puzzle_input = load_input("day00")
+    puzzle_input = load_input("day00").read()
     puzzle_solution = part1(puzzle_input)
     print(f"Part1: {puzzle_solution}")
     puzzle_solution = part2(puzzle_input)
